995594171d264a58d0d49d8216d0adeff87d5c35
sharaizgx <sharaiz123@gmail.com>
2017-04-11 14:22:50 +0200

    design form

--- form app/Parkeermeister/Parkeermeister/Main.Designer.cs
+++ form app/Parkeermeister/Parkeermeister/Main.Designer.cs
@@ -32,16 +32,21 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.Betaalmethode = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.contant = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pinmastercard = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
+            this.placeholdertarief = new System.Windows.Forms.Label();
+            this.laatstbijgewerkt = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.adressplaceholder = new System.Windows.Forms.Label();
+            this.adress = new System.Windows.Forms.Label();
+            this.placeholderbeschikbaar = new System.Windows.Forms.Label();
+            this.tarief = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -68,119 +73,175 @@
             // Betaalmethode
             // 
             this.Betaalmethode.AutoSize = true;
+            this.Betaalmethode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
             this.Betaalmethode.Location = new System.Drawing.Point(41, 471);
             this.Betaalmethode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Betaalmethode.Name = "Betaalmethode";
-            this.Betaalmethode.Size = new System.Drawing.Size(103, 17);
+            this.Betaalmethode.Size = new System.Drawing.Size(135, 22);
             this.Betaalmethode.TabIndex = 2;
-            this.Betaalmethode.Text = "Betaalmethode";
+            this.Betaalmethode.Text = "Betaalmethode:";
             // 
-            // label2
+            // contant
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 499);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.contant.AutoSize = true;
+            this.contant.Location = new System.Drawing.Point(42, 499);
+            this.contant.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.contant.Name = "contant";
+            this.contant.Size = new System.Drawing.Size(64, 17);
+            this.contant.TabIndex = 3;
+            this.contant.Text = "- contant";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(45, 122);
+            this.pictureBox1.Location = new System.Drawing.Point(45, 89);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(624, 310);
+            this.pictureBox1.Size = new System.Drawing.Size(608, 343);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // label1
+            // pinmastercard
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 528);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.pinmastercard.AutoSize = true;
+            this.pinmastercard.Location = new System.Drawing.Point(42, 528);
+            this.pinmastercard.Name = "pinmastercard";
+            this.pinmastercard.Size = new System.Drawing.Size(111, 17);
+            this.pinmastercard.TabIndex = 5;
+            this.pinmastercard.Text = "- pin/mastercard";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(765, 131);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label3.Location = new System.Drawing.Point(685, 82);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(70, 22);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "Details:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(765, 161);
+            this.label4.Location = new System.Drawing.Point(686, 118);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(90, 17);
             this.label4.TabIndex = 7;
-            this.label4.Text = "label4";
+            this.label4.Text = "Beschikbaar:";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(768, 197);
+            this.progressBar1.Location = new System.Drawing.Point(689, 157);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(231, 23);
+            this.progressBar1.Size = new System.Drawing.Size(220, 23);
             this.progressBar1.TabIndex = 8;
             this.progressBar1.Value = 60;
             // 
-            // label5
+            // placeholdertarief
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(768, 247);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "label5";
+            this.placeholdertarief.AutoSize = true;
+            this.placeholdertarief.Location = new System.Drawing.Point(689, 243);
+            this.placeholdertarief.Name = "placeholdertarief";
+            this.placeholdertarief.Size = new System.Drawing.Size(128, 17);
+            this.placeholdertarief.TabIndex = 10;
+            this.placeholdertarief.Text = "(tarief placeholder)";
             // 
-            // label6
+            // laatstbijgewerkt
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(768, 285);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "label6";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(686, 415);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 11;
-            this.label7.Text = "label7";
+            this.laatstbijgewerkt.AutoSize = true;
+            this.laatstbijgewerkt.Location = new System.Drawing.Point(660, 415);
+            this.laatstbijgewerkt.Name = "laatstbijgewerkt";
+            this.laatstbijgewerkt.Size = new System.Drawing.Size(133, 17);
+            this.laatstbijgewerkt.TabIndex = 11;
+            this.laatstbijgewerkt.Text = "laatst bijgewerkt op:";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(612, 528);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(515, 305);
+            this.pictureBox2.Size = new System.Drawing.Size(500, 300);
             this.pictureBox2.TabIndex = 12;
             this.pictureBox2.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label8.Location = new System.Drawing.Point(609, 499);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(208, 22);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Activiteit afgelopen week";
+            // 
+            // adressplaceholder
+            // 
+            this.adressplaceholder.AutoSize = true;
+            this.adressplaceholder.Location = new System.Drawing.Point(44, 619);
+            this.adressplaceholder.Name = "adressplaceholder";
+            this.adressplaceholder.Size = new System.Drawing.Size(148, 17);
+            this.adressplaceholder.TabIndex = 14;
+            this.adressplaceholder.Text = "- (adress placeholder)";
+            // 
+            // adress
+            // 
+            this.adress.AutoSize = true;
+            this.adress.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.adress.Location = new System.Drawing.Point(41, 583);
+            this.adress.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.adress.Name = "adress";
+            this.adress.Size = new System.Drawing.Size(71, 22);
+            this.adress.TabIndex = 16;
+            this.adress.Text = "Adress:";
+            // 
+            // placeholderbeschikbaar
+            // 
+            this.placeholderbeschikbaar.AutoSize = true;
+            this.placeholderbeschikbaar.Location = new System.Drawing.Point(785, 118);
+            this.placeholderbeschikbaar.Name = "placeholderbeschikbaar";
+            this.placeholderbeschikbaar.Size = new System.Drawing.Size(231, 17);
+            this.placeholderbeschikbaar.TabIndex = 9;
+            this.placeholderbeschikbaar.Text = "(beschikbare plaatsen placeholder)";
+            // 
+            // tarief
+            // 
+            this.tarief.AutoSize = true;
+            this.tarief.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.tarief.Location = new System.Drawing.Point(688, 200);
+            this.tarief.Name = "tarief";
+            this.tarief.Size = new System.Drawing.Size(62, 22);
+            this.tarief.TabIndex = 6;
+            this.tarief.Text = "Tarief:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(799, 415);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "(datum placeholder)";
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 852);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.adress);
+            this.Controls.Add(this.adressplaceholder);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.laatstbijgewerkt);
+            this.Controls.Add(this.placeholdertarief);
+            this.Controls.Add(this.placeholderbeschikbaar);
             this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tarief);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pinmastercard);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.contant);
             this.Controls.Add(this.Betaalmethode);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -202,16 +263,21 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label Betaalmethode;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label contant;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label pinmastercard;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label placeholdertarief;
+        private System.Windows.Forms.Label laatstbijgewerkt;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label adressplaceholder;
+        private System.Windows.Forms.Label adress;
+        private System.Windows.Forms.Label placeholderbeschikbaar;
+        private System.Windows.Forms.Label tarief;
+        private System.Windows.Forms.Label label1;
     }
 }
 
