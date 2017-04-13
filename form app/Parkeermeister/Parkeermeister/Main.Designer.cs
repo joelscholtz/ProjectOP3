60ec79df8b56e2f2a635d656da46367c80e5ff12
StefanDeWitHR <stefan_de_wit@outlook.com>
2017-04-13 11:40:01 +0200

    Google maps + listbox implemented
    Jordan & Fabian & Stefan

--- form app/Parkeermeister/Parkeermeister/Main.Designer.cs
+++ form app/Parkeermeister/Parkeermeister/Main.Designer.cs
@@ -29,11 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.Betaalmethode = new System.Windows.Forms.Label();
             this.contant = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
@@ -46,33 +43,16 @@
             this.placeholderbeschikbaar = new System.Windows.Forms.Label();
             this.tarief = new System.Windows.Forms.Label();
             this.datum_placeholder = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.ListBoxParking = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(191, 15);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(477, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(45, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(137, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "search";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Betaalmethode
             // 
             this.Betaalmethode.AutoSize = true;
             this.Betaalmethode.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.Betaalmethode.Location = new System.Drawing.Point(41, 471);
+            this.Betaalmethode.Location = new System.Drawing.Point(688, 216);
             this.Betaalmethode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Betaalmethode.Name = "Betaalmethode";
             this.Betaalmethode.Size = new System.Drawing.Size(135, 22);
@@ -82,28 +62,18 @@
             // contant
             // 
             this.contant.AutoSize = true;
-            this.contant.Location = new System.Drawing.Point(42, 499);
+            this.contant.Location = new System.Drawing.Point(689, 250);
             this.contant.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.contant.Name = "contant";
             this.contant.Size = new System.Drawing.Size(64, 17);
             this.contant.TabIndex = 3;
             this.contant.Text = "- contant";
             // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(45, 89);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(608, 343);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.label3.Location = new System.Drawing.Point(685, 82);
+            this.label3.Location = new System.Drawing.Point(680, 25);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(70, 22);
             this.label3.TabIndex = 6;
@@ -112,7 +82,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(686, 118);
+            this.label4.Location = new System.Drawing.Point(681, 71);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 17);
             this.label4.TabIndex = 7;
@@ -120,7 +90,7 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(689, 157);
+            this.progressBar1.Location = new System.Drawing.Point(684, 104);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(220, 23);
             this.progressBar1.TabIndex = 8;
@@ -128,7 +98,7 @@
             // placeholdertarief
             // 
             this.placeholdertarief.AutoSize = true;
-            this.placeholdertarief.Location = new System.Drawing.Point(689, 243);
+            this.placeholdertarief.Location = new System.Drawing.Point(689, 176);
             this.placeholdertarief.Name = "placeholdertarief";
             this.placeholdertarief.Size = new System.Drawing.Size(128, 17);
             this.placeholdertarief.TabIndex = 10;
@@ -137,7 +107,7 @@
             // laatstbijgewerkt
             // 
             this.laatstbijgewerkt.AutoSize = true;
-            this.laatstbijgewerkt.Location = new System.Drawing.Point(660, 415);
+            this.laatstbijgewerkt.Location = new System.Drawing.Point(672, 351);
             this.laatstbijgewerkt.Name = "laatstbijgewerkt";
             this.laatstbijgewerkt.Size = new System.Drawing.Size(133, 17);
             this.laatstbijgewerkt.TabIndex = 11;
@@ -185,7 +155,7 @@
             // placeholderbeschikbaar
             // 
             this.placeholderbeschikbaar.AutoSize = true;
-            this.placeholderbeschikbaar.Location = new System.Drawing.Point(785, 118);
+            this.placeholderbeschikbaar.Location = new System.Drawing.Point(777, 71);
             this.placeholderbeschikbaar.Name = "placeholderbeschikbaar";
             this.placeholderbeschikbaar.Size = new System.Drawing.Size(231, 17);
             this.placeholderbeschikbaar.TabIndex = 9;
@@ -195,7 +165,7 @@
             // 
             this.tarief.AutoSize = true;
             this.tarief.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.tarief.Location = new System.Drawing.Point(688, 200);
+            this.tarief.Location = new System.Drawing.Point(680, 140);
             this.tarief.Name = "tarief";
             this.tarief.Size = new System.Drawing.Size(62, 22);
             this.tarief.TabIndex = 6;
@@ -204,17 +174,37 @@
             // datum_placeholder
             // 
             this.datum_placeholder.AutoSize = true;
-            this.datum_placeholder.Location = new System.Drawing.Point(799, 415);
+            this.datum_placeholder.Location = new System.Drawing.Point(811, 351);
             this.datum_placeholder.Name = "datum_placeholder";
             this.datum_placeholder.Size = new System.Drawing.Size(135, 17);
             this.datum_placeholder.TabIndex = 17;
             this.datum_placeholder.Text = "(datum placeholder)";
+            // 
+            // ListBoxParking
+            // 
+            this.ListBoxParking.FormattingEnabled = true;
+            this.ListBoxParking.ItemHeight = 16;
+            this.ListBoxParking.Location = new System.Drawing.Point(-6, 4);
+            this.ListBoxParking.Name = "ListBoxParking";
+            this.ListBoxParking.ScrollAlwaysVisible = true;
+            this.ListBoxParking.Size = new System.Drawing.Size(248, 532);
+            this.ListBoxParking.TabIndex = 18;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(249, 13);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(403, 469);
+            this.webBrowser1.TabIndex = 19;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1139, 741);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.ListBoxParking);
             this.Controls.Add(this.datum_placeholder);
             this.Controls.Add(this.adress);
             this.Controls.Add(this.adressplaceholder);
@@ -227,17 +217,13 @@
             this.Controls.Add(this.tarief);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.contant);
             this.Controls.Add(this.Betaalmethode);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ParkeerMeister ™";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -245,12 +231,8 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label Betaalmethode;
         private System.Windows.Forms.Label contant;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ProgressBar progressBar1;
@@ -263,6 +245,8 @@
         private System.Windows.Forms.Label placeholderbeschikbaar;
         private System.Windows.Forms.Label tarief;
         private System.Windows.Forms.Label datum_placeholder;
+        private System.Windows.Forms.ListBox ListBoxParking;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- form app/Parkeermeister/Parkeermeister/Main.cs
+++ form app/Parkeermeister/Parkeermeister/Main.cs
@@ -28,30 +28,26 @@ namespace Parkeermeister
             StartForm();
         }
 
-        public class Vector2
-        {
-            double x, y;
-            public Vector2( double x , double y )
-            {
-                this.x = x;
-                this.y = y;
-            }
-        }
+    
 
         public void StartForm()
         {
             // make instance of object API
             API api = new API();
-
+            IList<JToken> parkingGaragesRaw;
             // dynamic url 
             JObject DynamicUrl = JObject.Parse(api.callApi("http://opendata.technolution.nl/opendata/parkingdata/v1/dynamic/0c9bc6de-95be-4803-8fb9-483b2291338d"));
             // static url
             JObject StaticUrl = JObject.Parse(api.callApi("http://opendata.technolution.nl/opendata/parkingdata/v1/static/650bc16f-d210-49f3-992b-530f9360b251"));
-
+            JObject allData = JObject.Parse(api.callApi("http://opendata.technolution.nl/opendata/parkingdata/v1"));
+           
+            parkingGaragesRaw = allData["parkingFacilities"].Children().ToList();
             // dynamic information
             var pFDI = DynamicUrl["parkingFacilityDynamicInformation"];
             // static information
-            var pFSI = DynamicUrl["parkingFacilityDynamicInformation"];
+            var pFSI     = StaticUrl["parkingFacilityStaticInformation"];
+
+            var PFAlldata = allData["parkingFacilities"];
             // static second array 
             var facStatus = pFDI["facilityActualStatus"];
             // indentiefier of parking url
@@ -62,18 +58,21 @@ namespace Parkeermeister
             double UnixTimeStamp = (int)facStatus["lastUpdated"];
             // function to convert unix time stamp to datetime
             DateTime unixTimestamp = UnixTimeStampToDateTime(UnixTimeStamp);
-           
-            // location required for Google maps API
-            Vector2 locationForDisplay = (string)pFSI["locationForDisplay"] != null ? locationForDisplay = new Vector2((double)pFSI["locationForDisplay"]["latitude"], (double)pFSI["locationForDisplay"]["longitude"]) : locationForDisplay = null;
 
+            // location required for Google maps API
+            string latitude =   pFSI["locationForDisplay"]  != null ? latitude =  (string) pFSI["locationForDisplay"]["latitude"]  : latitude = "";
+            string longitude =  pFSI["locationForDisplay"]  != null ? longitude = (string) pFSI["locationForDisplay"]["longitude"] : longitude = "";
             // check available spots and return an color
             if (available_spots > 50)
             {
                 placeholderbeschikbaar.BackColor = Color.Green;
-            }else if (available_spots < 50)   {
+            }
+            else if (available_spots < 50)
+            {
 
                 placeholderbeschikbaar.BackColor = Color.Orange;
-            }else if(available_spots == 0)
+            }
+            else if (available_spots == 0)
             {
                 placeholderbeschikbaar.BackColor = Color.Red;
             }
@@ -92,15 +91,43 @@ namespace Parkeermeister
                 placeholdertarief.Visible = false;
             }
             // Payment methods 
-            contant.Text = (string)pFSI["paymentMethods"] != null ? contant.Text = (string)pFSI["paymentMethods"].ToString() : contant.Text = "Geen betaalmethodes aangegeven";
-           
-
+            contant.Text = pFSI["paymentMethods"] != null ? contant.Text = (string)pFSI["paymentMethods"].ToString() : contant.Text = "Geen betaalmethodes aangegeven";
 
             // progress bar 
             int total_spaces = (int)facStatus["parkingCapacity"];
-            int free_spaces =  (int)facStatus["vacantSpaces"];
-            float  total = ((float)free_spaces / (float)total_spaces) * 100;
-            progressBar1.Value = 100 - (int) total;
+            int free_spaces = (int)facStatus["vacantSpaces"];
+            float total = ((float)free_spaces / (float)total_spaces) * 100;
+            progressBar1.Value = 100 - (int)total;
+
+            // showing all parking garages
+            foreach (JToken jt in parkingGaragesRaw)
+            {
+                ListBoxParking.Items.Add((string)jt["name"]);
+                // ListBoxParking.MouseDoubleClick += new MouseEventHandler(ListBoxOnClick(jt["identiefier"]));
+
+            }
+            try
+            {
+                // A queryadress must be properly
+                StringBuilder queryadress = new StringBuilder();
+                queryadress.Append("https://www.google.nl/maps/@" + latitude + ","+ longitude + ",15z");
+           
+                webBrowser1.Navigate(queryadress.ToString());
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+
+        private void ListBoxOnClick(object sender, EventArgs e , string identiefer)
+        {
+            if (ListBoxParking.SelectedItem != null)
+            {
+                
+            }
         }
         public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
